340a24f545d1f1579f4ceb59cf548b165df241bb
romulofgouvea <romulo.gouvea.lemaf@gmail.com>
2019-09-19 14:27:52 -0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+function Index() {
+
+    return (
+        <Text>Aqui</Text>
+    )
+}
+
+export default Index;
---
+++ src/pages/Explore/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from "react-native";
+
+const Explore = () => {
+  return (
+    <View>
+      <Text> Hello Word</Text>
+    </View>
+  );
+};
+
+export default Explore;
